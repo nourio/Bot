ba8ca965f89f07b88ac84f439cf7eef1c40895f3
SlovaBot <35879102+SlovaBot@users.noreply.github.com>
2018-01-27 23:28:54 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name='bot',
+        packages=['bot'],
+        include_package_data=True,
+        install_requires=[
+            'discord.py'
+        ],
+)
